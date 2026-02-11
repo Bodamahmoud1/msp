3d689ee2209afed22d9fd842b8a7bf8b1b9e2183
Mushroom <hilariousbananas@proton.me>
2026-02-11 20:55:09 +0200

    simple-user-pass-brute v2

--- simple-user-pass-brute/controllers/authController.js
+++ simple-user-pass-brute/controllers/authController.js
@@ -6,11 +6,11 @@ function login(req, res) {
 
   const user = findUser(username);
   if (!user) {
-    return res.status(404).json({ ok: false, message: "username not found" });
+    return res.status(404).json({ ok: false, message: "wrong credentials" });
   }
 
   if (password !== user.password) {
-    return res.status(401).json({ ok: false, message: "invalid password" });
+    return res.status(401).json({ ok: false, message: "wrong credentials" });
   }
 
   return res.json({ ok: true, message: "access granted", flag: `Login successful: ${user.flag}` });
